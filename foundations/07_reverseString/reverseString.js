9b6e5b01e09c43cdf18590571a4309db8dab421c
nae-hp <259297935+naelimbu-dot@users.noreply.github.com>
2026-07-12 16:13:05 +0545

    Did exercise 7

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    arr = text.split("")
+    let length = text.length
+    let newArr = []
+    for (let i = length - 1; i > -1; i--) {
+        newArr.push(arr[i])
+    }
+    return newArr.join("")
 };
 
 // Do not edit below this line
