00b127640ecf634f14dc519017a8867064295a87
nae-hp <259297935+naelimbu-dot@users.noreply.github.com>
2026-07-12 16:05:42 +0545

    Did exercises 1-6

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -9,7 +9,15 @@
 
 // Add your code right below, good luck!
 
+firstName = "Carlos"
+lastName = "Stevenson"
+thisYear = 1965
+birthYear = 1947
+age = thisYear - birthYear
+fullName = firstName + " " + lastName
 
+greeting = `Hello! My name is ${fullName} and I am ${age} years old.`
+console.log(this.greeting)
 
 
 
--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(text, num) {
+    let myString = ""
+    if (num < 0) {
+        return "ERROR"
+    } else {
+        for (let i = 0; i < num; i++) {
+            myString += text
+        }
+    }
+    return myString
 };
 
 // Do not edit below this line
