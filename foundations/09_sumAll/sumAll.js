71ba9ac372b901707c91eff3c81a1f0274275e05
nae-hp <259297935+naelimbu-dot@users.noreply.github.com>
2026-07-12 19:44:11 +0545

    Did exercise 9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0
+    if (typeof(start) !== "number" || 
+    typeof(end) !== "number" || 
+    start < 0 || 
+    end < 0 || 
+    start !== Math.floor(start) || 
+    end !== Math.floor(end)) {
+        return "ERROR"
 
+    } else if (end > start) {
+        for (let i = start; i <= end; i++) {
+        sum += i
+    } 
+    } else {
+        for (let i = end; i <= start; i++) {
+        sum += i
+    } 
+    }
+    return sum
 };
 
 // Do not edit below this line
