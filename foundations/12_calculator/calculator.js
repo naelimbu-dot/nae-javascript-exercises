c978edd07cd1bd562fccc958dc3932da5f6a7357
nae-hp <259297935+naelimbu-dot@users.noreply.github.com>
2026-07-16 18:49:36 +0545

    Did exercise 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((current, num) => current + num, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((current, num) => current * num, 1)
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let i = num
+	while (i > 1) {
+    i--
+    num = num * i
+  }
+  if (num === 0) {
+    return 1
+  }
+  return num
 };
 
 // Do not edit below this line
