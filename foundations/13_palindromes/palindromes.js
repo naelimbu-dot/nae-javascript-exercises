ef909fa99e9828d52f8a6f559ed922e747fac62a
nae-hp <259297935+naelimbu-dot@users.noreply.github.com>
2026-07-16 19:48:06 +0545

    Did exercise 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    const dictionary = "abcdefghijklmnopqrstuvwxyz1234567890"
+    let newString = text
+        .toLowerCase()
+        .split("")
+        .filter((letter) => dictionary.includes(letter))
+        .join("")
+    
+    const reversedString = newString.split('').reverse().join('')
+    return newString === reversedString;
+}; 
 
-};
 
 // Do not edit below this line
 module.exports = palindromes;
