c15e47b44788d92e6b1a75a1825090d30c0cfdbe
nae-hp <259297935+naelimbu-dot@users.noreply.github.com>
2026-07-16 20:38:21 +0545

    Did exercise 16 (LAST)

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const oldest = arr.sort((prev, next) => {
+        if (typeof(prev.yearOfDeath) !== "number") {
+            prev.yearOfDeath = new Date().getFullYear()
+        } if (typeof(next.yearOfDeath) !== "number") {
+            next.yearOfDeath = new Date().getFullYear()
+        }
+        return (next.yearOfDeath - next.yearOfBirth) - (prev.yearOfDeath - prev.yearOfBirth)
+    })
+    return oldest[0]
 };
 
 // Do not edit below this line
