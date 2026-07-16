d210cf4b75d4a19f684130ab6541e29233ce5a1e
nae-hp <259297935+naelimbu-dot@users.noreply.github.com>
2026-07-16 20:00:24 +0545

    Did exercise 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(nth) {
+    if (nth === 0 || nth === "0") {
+        return 0
+    } 
+    if (nth < 0) {
+        return "OOPS"
+    }
+    let prev = 0
+    let num = 1
+    let temp = 0
+    for (let i = 1; i < nth; i++) {
+        temp = num
+        num += prev
+        prev = temp
+    }
+    return num
 };
 
 // Do not edit below this line
